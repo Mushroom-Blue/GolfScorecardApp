485101900e9fb417b8f3f318b15e05aba1f4904b
Taft Miller <taftmiller@Tafts-MacBook-Pro.local>
2021-11-04 18:38:50 -0600

    filled out more JS, started landing page

---
+++ src/main.js
@@ -0,0 +1,22 @@
+let golfAPI = "https://golf-courses-api.herokuapp.com/courses/18300";
+let oneCourse;
+
+let xmlHTTP = new XMLHttpRequest();
+
+xmlHTTP.onreadystatechange = () => {
+    if (xmlHTTP.readyState === 4 && xmlHTTP.status === 200) {
+        oneCourse = JSON.parse(xmlHTTP.responseText);
+        console.log(oneCourse);
+        document.getElementById("oneCourseIMG").innerHTML = `
+            <img class="courseIMG" src='${oneCourse.data.thumbnail}'/>
+        `
+        document.getElementById("oneCourseIMGTXT").innerHTML = `
+            <div class="courseName">${oneCourse.data.name}</div>
+            <div class="coursePhone">${oneCourse.data.phone}</div>
+        `
+    }
+}
+xmlHTTP.open("GET", golfAPI, true);
+xmlHTTP.setRequestHeader("ContentType", "application/json");
+xmlHTTP.send();
+
