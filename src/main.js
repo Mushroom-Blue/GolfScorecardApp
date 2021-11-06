cb71876f6db3996d197aa84ac0c8cd7ac7a0f670
Taft Miller <taftmiller@Tafts-MacBook-Pro.local>
2021-11-05 20:05:38 -0600

    added more JS, finished landing page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 let golfAPI = "https://golf-courses-api.herokuapp.com/courses/18300";
 let oneCourse;
+let proTotal = 0;
+let champTotal = 0;
+let menTotal = 0;
+let womenTotal = 0;
+let selectedTee;
+let savedData = [];
 
 let xmlHTTP = new XMLHttpRequest();
 
@@ -17,9 +23,61 @@ xmlHTTP.onreadystatechange = () => {
             <div class="courseCity">${oneCourse.data.city}</div>
             <div class="courseState">${oneCourse.data.stateOrProvince}</div>
         `
+        totalTee();
+        document.getElementById("teeType").innerHTML = `
+            <option value="pro">Pro - ${proTotal} yds</option>
+            <option value="champion">Champion - ${champTotal} yds</option>
+            <option value="men">Men - ${menTotal} yds</option>
+            <option value="women">Women - ${womenTotal} yds</option>
+        `
+        currentTee();
     }
 }
 xmlHTTP.open("GET", golfAPI, true);
 xmlHTTP.setRequestHeader("ContentType", "application/json");
 xmlHTTP.send();
 
+function totalTee() {
+    oneCourse.data.holes.forEach((eachHole) => {
+        proTotal += eachHole.teeBoxes[0].yards
+        champTotal += eachHole.teeBoxes[1].yards
+        menTotal += eachHole.teeBoxes[2].yards
+        womenTotal += eachHole.teeBoxes[3].yards
+    })
+}
+
+function currentTee() {
+    selectedTee = document.getElementById("teeType").value;
+}
+
+function addPlayerNames() {
+    let savedPlayerNames = document.getElementById("playerNameInput").value;
+    let playerNamesList = document.getElementById("playerNames");
+    if (savedPlayerNames === "") return;
+    if (savedData.includes(savedPlayerNames.toLowerCase())){
+        document.getElementById("errorMsg").innerHTML = "Player Already Exists";
+        document.getElementById("playerNameInput").value = "";
+        setTimeout(()=>{
+            document.getElementById("errorMsg").innerHTML = "";
+        }, 5000)
+        return
+    }
+    if (savedData.length === 4){
+        document.getElementById("errorMsg").innerHTML = "Player Roster Is Full!";
+        document.getElementById("playerNameInput").value = "";
+        setTimeout(()=>{
+            document.getElementById("errorMsg").innerHTML = "";
+        }, 5000)
+        return
+    }
+    savedData.push(savedPlayerNames.toLowerCase());
+    document.getElementById("playerNameInput").value = "";
+    console.log(savedData)
+    playerNamesList.insertAdjacentHTML(
+        "beforeend", 
+        `
+        <div class="singlePlayer">${savedPlayerNames}</div>
+    `
+    )
+    
+}
